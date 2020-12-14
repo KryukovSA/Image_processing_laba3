69162c99bc7a8362178fd346bfe27ef6a9e23584
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-12-14 14:42:56 +0300

    first

--- part_1.py
+++ part_1.py
@@ -1,2 +1,9 @@
+import cv2
+import os.path as osp
+
+from image_processing_lib.cli_image_argument import get_image_path
+from image_processing_lib.windows_manager import create_two_windows
+from image_processing_lib.time_comparing import get_time
+
 if __name__ == "__main__":
     print('Hello world!')
